20ab7f09b6423431a2cb1aa2aff8a9948cb6cba5
gmfub <ivanholovko71@gmail.com>
2026-05-09 15:27:31 +0200

    more wallets

--- wallet-connect/src/wagmi.js
+++ wallet-connect/src/wagmi.js
@@ -1,10 +1,31 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, base } from 'wagmi/chains';
+import { mainnet, base, polygon, optimism, arbitrum } from 'wagmi/chains';
+import {
+    injectedWallet,
+    rainbowWallet,
+    walletConnectWallet,
+    coinbaseWallet,
+    trustWallet,
+    ledgerWallet
+} from '@rainbow-me/rainbowkit/wallets';
 
 export const config = getDefaultConfig({
     appName: 'wallet-connect',
     projectId: 'e21c759ddabfdee2fce1c526b96833cd',
-    chains: [mainnet, base],
+    chains: [mainnet, base, polygon, optimism, arbitrum],
     ssr: false,
+    wallets: [
+        {
+            groupName: 'Popular',
+            wallets: [
+                injectedWallet,
+                rainbowWallet,
+                coinbaseWallet,
+                walletConnectWallet,
+                trustWallet,
+                ledgerWallet,
+            ],
+        },
+    ],
 });
